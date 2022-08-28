5422d835d4a0bd903d5f5108f9553a7a13714b2c
Kody Low <74332828+Kodylow@users.noreply.github.com>
2022-08-28 05:41:10 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello, from Flask!'
+    if request.method == 'POST':
+        
+    else:
+        return render_template('index.html')
 
 app.run(host='0.0.0.0', port=81, debug=True)
